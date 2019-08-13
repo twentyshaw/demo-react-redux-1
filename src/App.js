f26408f6290deb0d160fd3dfcfcb8c166f56525c
twentyshaw <isherlock951106@gmail.com>
2019-08-13 14:16:05 +0900

    点击按钮加1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
 class App extends React.Component{
-  add1(){
-    this.props.onAdd1()
-  }
-  add2(){
-    this.props.onAdd2()
-  }
-  add3(){
-    this.props.onAdd3()
-  }
-  add4(){
-    this.props.onAdd4()
-  }
   render(){
     return (
     <div>
-      你点击了<span id="value">{this.props.value}</span>次
+      你点击了<span id="value">{this.props.n}</span>次
       <div>
-        <button id="add1" onClick={this.add1.bind(this)}>+1</button>
-        <button id="add2" onClick={this.add2.bind(this)}>+2</button>
-        <button id="addIfOdd" onClick={this.add3.bind(this)}>如果是单数就+1</button>
-        <button id="add1After2Sec" onClick={this.add4.bind(this)}>两秒后+1</button>
+        <button id="add1" onClick={this.props.add1.bind(this)}>+1</button>
+        <button id="add2" onClick={this.props.add2.bind(this)}>+2</button>
+        <button id="addIfOdd">如果是单数就+1</button>
+        <button id="add1After2Sec">两秒后+1</button>
       </div>
     </div>
   )};
 }
 
-export default App;
+function mapStateToProps(state){ //必须是函数
+  return {
+    n: state.n     //从当前的state中取出你想要的值 ： n; x专门用来生成props
+  }
+}
+
+function mapDispatchToProps(dispatch){ 
+    return{
+      add1: ()=>dispatch({type:'add', payload:1}),
+      add2: ()=>dispatch({type:'add', payload:2}),
+    }
+}
+
+// connect 就是把 x与 App结合起来？ 
+export default connect(mapStateToProps,mapDispatchToProps)(App);// export connect以后的App； connect 接受两个参数
